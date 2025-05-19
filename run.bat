e31708cd67be7f7ab44f9fa80b5ff6a96ce426b1
Administrator <dms@uke>
2025-05-19 13:19:47 +0800

    。。。

---
+++ run.bat
@@ -0,0 +1,8 @@
+
+
+cd C:\Users\Administrator\IdeaProjects\kCandlPrj
+set java_home="C:\Users\Administrator\.jdks\jbr-21.0.7"
+
+"C:\Program Files\JetBrains\IntelliJ IDEA 2025.1.1.1\plugins\maven\lib\maven3\bin\mvn.cmd" exec:java -Dexec.mainClass="org.example.Wbsvr"
+pause
+pause
